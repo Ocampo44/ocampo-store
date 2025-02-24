195380bd058366fe2160d90fdb09a2a8ca297f60
Ocampo44 <diegoocampo2344@gmail.comm>
2025-02-24 11:24:13 -0600

    -

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -225,23 +225,6 @@ const Sidebar = () => {
             </svg>
             {isOpen && <span className="text-lg">Ventas</span>}
           </Link>
-
-          {/* Compras2 */}
-          <Link
-            to="/compras2"
-            className="flex items-center gap-4 px-4 py-3 hover:bg-indigo-600 transition-colors"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V3a1 1 0 00-1-1H9a1 1 0 00-1 1v8M5 11h14l-1 9H6l-1-9z" />
-            </svg>
-            {isOpen && <span className="text-lg">Compras2</span>}
-          </Link>
         </nav>
 
         {/* Pie */}
